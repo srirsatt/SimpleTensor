6c17090b38ec1e190de726b4fc76d1efe399c615
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2026-06-03 10:26:08 -0500

    intro to cudaMalloc, cudaFree, and cudaMemcpy

--- hello.cu
+++ hello.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h> // cuda mem alloc
 #include <stdio.h> // printf
 #include <stdlib.h> // malloc C-compatoat* 
+#include <iostream>
 
 
 __global__ void helloKernel() {
@@ -30,17 +31,68 @@ __global__ void parallelAdd(float* a, float* b, float* c) {
 
     c[i] = a[i] + b[i];
 
-    printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d", blockIdx.x, blockDim.x, threadIdx.x);
+    //printf("blockIdx.x: %d, blockDim.x: %d, threadIdx.x: %d", blockIdx.x, blockDim.x, threadIdx.x);
+    //printf("c[i] is %f", c[i]);
 
 }
 
 int main() {
-    helloKernel<<<3, 32>>>();
+    //helloKernel<<<3, 32>>>();
+
+    // lets setup GPU arrays and malloc / free / cudaMemCpy for parallelAdd()
+
+    // need a var to declare size of elements
+    int N = 96;
+
+    float cpu_a[N];
+    float cpu_b[N];
+    // first - make CPU memory
+    for (int j = 0; j < 96; j++) {
+        cpu_a[j] = 1;
+        cpu_b[j] = 1;
+    }
+    float cpu_c[96]; // copy back from d_c to cpu_c later using cudaMemcpy
+
+
+
+    float* d_a;
+    cudaMalloc(&d_a, N * sizeof(float)); // object ref, size of arr
+
+    float* d_b;
+    cudaMalloc(&d_b, N * sizeof(float));
+
+    float* d_c;
+    cudaMalloc(&d_c, N*sizeof(float));
+
+    // copy elements via cudaMemCpy
+
+    cudaMemcpy(d_a, cpu_a, N*sizeof(float), cudaMemcpyHostToDevice); // cpu to gpu mem copy
+    cudaMemcpy(d_b, cpu_b, N*sizeof(float), cudaMemcpyHostToDevice); // arr b copy
+
+    // run kernel
+
+    parallelAdd<<<3, 32>>>(d_a, d_b, d_c);
+
+    // copy back over
+
+    cudaMemcpy(cpu_c, d_c, N*sizeof(float), cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < 96; i++) {
+        // print of cpu_c back to me
+        std::cout << cpu_c[i] << std::endl;
+    }
 
     cudaDeviceSynchronize();
 
     // parallelAdd(a, b, c); // going thru cudaMalloc & cudaFree through this later
     // simple print
+
+    // free all ur memory
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
 }
 
 // compile - nvcc hello.cu -o hello - compiles to hello executable
