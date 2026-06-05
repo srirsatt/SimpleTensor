c7e52f76bbb78dad99fc492d9af1882e4a4cc9ff
Sriram Sattiraju <sriramsattiraju07@gmail.com>
2026-06-05 16:23:00 -0500

    updated tensor.h + tensor.cu skeleton

--- tensor/tensor.cu
+++ tensor/tensor.cu
@@ -1,2 +1,45 @@
 // basic tensor implementation
 
+// tensor should have:
+
+/*
+
+Constructor
+Destructor
+1. Data Buffer (vector)
+2. Shape (3 element arr int)
+3. Stride (3 element arr int) - calculated from shape
+
+getters & setters, of course!
+*/
+
+#include "tensor.h"
+#include <cuda_runtime.h>
+#include <iostream>
+#include <vector>
+
+
+SimpleTensor::SimpleTensor(int* shape, int dimension) {
+    // constructor
+}
+
+SimpleTensor::~SimpleTensor() {
+    // destructor
+}
+
+void SimpleTensor::setShape(int* shape, int dimension) {
+
+}
+
+std::vector<int> SimpleTensor::getShape() {
+
+}
+
+float* SimpleTensor::getBuffer() {
+
+}
+
+std::vector<int> SimpleTensor::getStride() {
+    
+}
+
--- tensor/tensor.h
+++ tensor/tensor.h
@@ -1,2 +1,37 @@
 // tensor.h
 
+/*
+Class Declaration
+
+Constructor
+Destructor
+1. Data Buffer (vector)
+2. Shape (3 element arr int)
+3. Stride (3 element arr int) - calculated from shape
+
+getters & setters, of course!
+*/
+
+#pragma once // replaces ifndef and define, endif
+#include <string>
+#include <vector>
+
+
+class SimpleTensor {
+    public:
+        SimpleTensor(int* shape, int dimension); // dimension for Dim of tensor
+        ~SimpleTensor();
+        // setters and getters
+        void setShape(int* shape, int dimension);
+        std::vector<int> getShape();
+        float* getBuffer();
+        std::vector<int> getStride();
+
+
+    private:
+        int dimension = 0;
+        int size = 1; // set with the constructor
+        float* dataBuffer; // CUDA memory
+        std::vector<int> shape; // CPU
+        std::vector<int> stride;
+};
